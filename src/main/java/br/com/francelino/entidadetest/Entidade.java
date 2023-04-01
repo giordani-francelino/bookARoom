311e45a76e14415c99d0d8075815c61b6bb39769
giordani-francelino <francelino.giordani@gmail.com>
2023-04-01 13:32:16 -0300

    reinclusao entidade

---
+++ src/main/java/br/com/francelino/entidadetest/Entidade.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.francelino.entidadetest;
+
+/**
+ *
+ * @author Your Name &lt;francelino at ifnmg&gt;
+ */
+public class Entidade {
+    
+}
