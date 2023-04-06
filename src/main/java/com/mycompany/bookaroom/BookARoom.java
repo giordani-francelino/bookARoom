74137535f30732c1a8b8621c5a72b7dda05f98aa
giordani-francelino <francelino.giordani@gmail.com>
2023-04-06 20:35:02 -0300

    inclusao de construtores

---
+++ src/main/java/com/mycompany/bookaroom/BookARoom.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bookaroom;
+
+/**
+ *
+ * @author Your Name &lt;francelino at ifnmg&gt;
+ */
+public class BookARoom {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
